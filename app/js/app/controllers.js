89705c21272bd828ed8e6b528c77e31ac85106af
Ryo Anazawa <anazawa@cpan.org>
2016-06-09 03:12:46 +0900

    second commit

--- app/js/app/controllers.js
+++ app/js/app/controllers.js
@@ -3,38 +3,68 @@
 angular.module("kgsApiExplorer.controllers", [
     "kgsApiExplorer.services" 
 ]).
-controller("requestCtrl", function ($scope, $http, kgsPoller) {
+controller("headerCtrl", function ($scope) {
+    var messages = [
+        "for testing!",
+        "for developing!",
+        "for fun!"
+    ];
+    $scope.message = messages[Math.floor(Math.random()*messages.length)];
+}).
+controller("upstreamCtrl", function ($scope, $http, kgsPoller) {
     var MESSAGE_TYPES = [
-        "LOGIN",
-        "LOGOUT"
+        "CHAT",
+        "DETAILS_JOIN_REQUEST",
+        "JOIN_ARCHIVE_REQUEST",
+        "JOIN_TAG_ARCHIVE_REQUEST",
+        "LOGOUT",
+        "REQUEST_SERVER_STATS",
+        "SYNC_REQUEST",
+        "UNJOIN_REQUEST",
+        "WAKE_UP"
     ];
     $http.get("data/locales.json").then(function (response) {
         $scope.locales = response.data;
     });
 
-    $scope.isPolling = false;
+    $scope.types = ["LOGIN"];
     $scope.message = { type: "LOGIN" };
     $scope.history = [];
-    $scope.types = MESSAGE_TYPES.slice(0, 1);
+    $scope.isSending = false;
+    $scope.error = "";
+
+    $scope.send = function (form) {
+        if (form.$valid) {
+            $scope.isSending = true;
+            kgsPoller.send($scope.message).then(function () {
+                $scope.isSending = false;
+                $scope.history.unshift($scope.message);
+                if (kgsPoller.isLoggedIn()) {
+                    $scope.message = { type: $scope.types[0] };
+                }
+                form.$setPristine();
+                form.$setUntouched();
+            }, function (xhr) {
+                $scope.error = xhr.status ? xhr.status+" "+xhr.statusText : "";
+            });
+        }
+    };
 
     kgsPoller.
     on("LOGIN_SUCCESS", function () {
-        $scope.types = MESSAGE_TYPES.slice(1);
+        $scope.types = MESSAGE_TYPES.slice(0);
         $scope.message = { type: $scope.types[0] };
     }).
     on("LOGOUT", function () {
-        $scope.types = MESSAGE_TYPES.slice(0, 1);
-        $scope.message = { type: $scope.types[0] };
+        $scope.types = ["LOGIN"];
+        $scope.message = { type: "LOGIN" };
     });
-
-    $scope.send = function () {
-        kgsPoller.send($scope.message);
-        $scope.history.unshift($scope.message);
-    };
 }).
-controller("responseCtrl", function ($scope, kgsPoller) {
+controller("downstreamCtrl", function ($scope, kgsPoller) {
     $scope.messages = [];
     $scope.isPolling = false;
+    $scope.error = "";
+
     kgsPoller.
     on("message", function (message) {
         $scope.messages.unshift(message);
@@ -44,8 +74,53 @@ controller("responseCtrl", function ($scope, kgsPoller) {
     }).
     on("LOGOUT", function () {
         $scope.isPolling = false;
+    }).
+    on("error", function (error) {
+        if (error.type === "kgsPollerPollingError") {
+            $scope.error = error.toString();
+        }
     });
 }).
-controller("summaryCtrl", function ($scope) {
+controller("channelSubscriptionsCtrl", function ($scope, kgsPoller) {
+    kgsPoller.
+    on("LOGIN_SUCCESS", function () {
+        $scope.channels = {};
+    }).
+    on("ROOM_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "ROOM"
+        };
+    }).
+    on("GAME_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "GAME"
+        };
+    }).
+    on("CONVO_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "ARCHIVE"
+        };
+    }).
+    on("ARCHIVE_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "ARCHIVE"
+        };
+    }).
+    on("TAG_ARCHIVE_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "TAG_ARCHIVE"
+        };
+    }).
+    on("DETAILS_JOIN", function (message) {
+        $scope.channels[message.channelId] = {
+            type: "DETAILS"
+        };
+    }).
+    on("UNJOIN", function (message) {
+        delete $scope.channels[message.channelId];
+    }).
+    on("LOGOUT", function () {
+        $scope.channels = null;
+    });
 });
 
--- app/js/app/services.js
+++ app/js/app/services.js
@@ -2,7 +2,7 @@
 
 angular.module("kgsApiExplorer.services", [
 ]).
-factory("kgsPoller", function ($log, $rootScope) {
+factory("kgsPoller", function ($log, $rootScope, $q) {
     var that = kgsPoller({
         logger: $log
     });
@@ -14,6 +14,15 @@ factory("kgsPoller", function ($log, $rootScope) {
             return ret;
         };
     }(that.emit));
+
+    that.send = (function (superSend) {
+        return function (message) {
+            var that = this;
+            return $q(function (resolve, reject) {
+                superSend.call(that, message, resolve, reject);
+            });
+        };
+    }(that.send));
 
     return that;
 });
